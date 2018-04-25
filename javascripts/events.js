7f060c03714f75c0e8c2cfe3356a2dd1e11ffae0
Leo Taylor <leotaylor723@gmail.com>
2018-04-25 09:20:07 -0500

    can uncheck box, thanks pop.()

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,10 +2,17 @@ const data = require('./data');
 const printToRightDom = require('./budgetDom');
 const checkBoxes = document.getElementsByClassName('checkbox');
 const elementSelected = [];
+const budgetButton = document.getElementsByClassName('btn');
 
 const addCheckEvents = () => {
   for (let i = 0; i < checkBoxes.length; i++) {
-    checkBoxes[i].addEventListener('change', checkItDontWreckIt);
+    checkBoxes[i].addEventListener('change', function (e) {
+      if (this.checked) {
+        checkItDontWreckIt(e);
+      } else {
+        wreckItDontCheckIt(e);
+      };
+    });
   };
 };
 
@@ -13,12 +20,31 @@ const checkItDontWreckIt = (e) => {
   data.getElements().forEach((item) => {
     if (e.target.id === item.id) {
       elementSelected.push(item);
-    }
+    };
   });
   printToRightDom(elementSelected);
   data.setChecked(elementSelected);
 };
 
+const wreckItDontCheckIt = (e) => {
+  data.getElements().forEach((item) => {
+    if (e.target.id !== item.id) {
+      elementSelected.pop(item);
+    };
+  });
+  printToRightDom(elementSelected);
+  data.setChecked(elementSelected);
+};
+
+const setBudgetEvent = () => {
+  budgetButton.addEventListener('click', budgetDontFudgeIt);
+};
+
+const budgetDontFudgeIt = (e) => {
+
+};
+
 module.exports = {
   addCheckEvents,
+  setBudgetEvent,
 };
